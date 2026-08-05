1ee0db1f4ff0c92ff7ff97798781d3e76d0c70c2
Wulandhika <wulandhikakurnaliawati@gmail.com>
2026-08-05 14:50:22 +0700

    komikkomik

--- models/komik.js
+++ models/komik.js
@@ -1,27 +1,43 @@
 module.exports = (sequelize, DataTypes) => {
-    const Komik = sequelize.define("Komik", {
-        id: {
+    const Komik = sequelize.define('Komik', {
+        id_komik: {
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
         },
-
-        judul: {
+        judul_komik: {
             type: DataTypes.STRING,
             allowNull: false
         },
-
-        sinopsis: {
+        sinopsis_komik: {
             type: DataTypes.TEXT,
-            allowNull: false
+            allowNull: true
         },
-
-        tahun_terbit: {
+        tahun_terbit_komik: {
             type: DataTypes.INTEGER,
             allowNull: false
         },
-
         penulis_id: {
             type: DataTypes.INTEGER,
             allowNull: false
-        },
+        }
+
+
+    }, {
+        tableName: 'komik',
+        timestamps: true
+    });
+    komik.associate = (models) => {
+        komik.belongsTo(models.Penulis, {
+            foreignKey: 'penulis_id',
+            as: 'penulis'
+        });
+        komik.belongsToMany(models.Genre, {
+            through: 'KomikGenre',
+            foreignKey: 'komik_id',
+            otherKey: 'genre_id',
+            as: 'genre'
+        });
+    };
+    return Komik;
+}
